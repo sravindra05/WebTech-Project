83b3da88b9de0ef2401eaa467816db8135ed1053
Sarang Ravindra <sarang7822ravindra@gmail.com>
2020-03-10 23:09:47 +0530

    fixes

---
+++ backend/classifier/bin_class.py
@@ -0,0 +1,8 @@
+import flask
+from flask_api import status
+from flask_cors import CORS
+import requests
+import pymongo
+from datetime import date
+import json
+import hashlib
--- frontend/src/components/DashBoard.js
+++ frontend/src/components/DashBoard.js
@@ -5,110 +5,140 @@ import { NavLink } from "react-router-dom";
 import gen_request from "../libfx/gen_request";
 
 class DashBoard extends React.Component {
-  
-  componentDidMount(){
-    var url = "http://localhost:4000"
-    function delete_file(file_to_delete){
-      let url = "http://localhost:5000"
+  componentDidMount() {
+    var url = "http://localhost:4000";
+    function delete_file(file_to_delete) {
+      let url = "http://localhost:5000";
       //console.log(file_to_delete.target.id);
-      gen_request("DELETE",url+"/api/user/v1/delete_file/"+file_to_delete.target.id,5000,{},after_delete);
-      function after_delete(){
-        if (this.readyState == 4){
-          if (this.status == 200){
+      gen_request(
+        "DELETE",
+        url + "/api/user/v1/delete_file/" + file_to_delete.target.id,
+        5000,
+        {},
+        after_delete
+      );
+      function after_delete() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
             alert("File deleted successfully");
             window.location.reload();
           }
         }
       }
     }
-    function load_table(){
-      let url = "http://localhost:5000"
-      gen_request("GET",url+"/api/user/v1/get_file_list",5000,{},callback);
-      function callback(){
-        if (this.readyState == 4){
-          if(this.status == 204){
-            document.getElementById("file_list").innerHTML = "<p>You currently have no life and no files";
+    function load_table() {
+      let url = "http://localhost:5000";
+      gen_request(
+        "GET",
+        url + "/api/user/v1/get_file_list",
+        5000,
+        {},
+        callback
+      );
+      function callback() {
+        if (this.readyState == 4) {
+          if (this.status == 204) {
+            document.getElementById("file_list").innerHTML =
+              "<p>You currently have no life and no files";
           }
-          if (this.status == 200){
-            let data  = JSON.parse(this.response);
-            let div =  document.getElementById("file_list");
-            for(var key in data){
-              let newdiv = document.createElement("div")
-              newdiv.className="row"
-              let newp = document.createElement('p');newp.style="display: inline-block";
-              newp.innerText = data[key]['filename'] + " File Size: "+data[key]['file_size'];
-              let newplot = document.createElement('a');newplot.className="btn white-text blue";newplot.style="display: inline-block;float:right";
-              let newlearn = document.createElement('a');newlearn.className="btn white-text pink";newlearn.style="display: inline-block;float:right";
-              let newdelete = document.createElement('a');newdelete.className="btn white-text red";newdelete.style="display: inline-block;float:right";
+          if (this.status == 200) {
+            let data = JSON.parse(this.response);
+            let div = document.getElementById("file_list");
+            for (var key in data) {
+              let newdiv = document.createElement("div");
+              newdiv.className = "col s12";
+              let newp = document.createElement("p");
+              newp.style = "display: inline-block";
+              newp.innerText =
+                data[key]["filename"] + " File Size: " + data[key]["file_size"];
+              let newplot = document.createElement("a");
+              newplot.className = "btn white-text blue";
+              newplot.style = "display: inline-block;float:right";
+              let newlearn = document.createElement("a");
+              newlearn.className = "btn white-text pink";
+              newlearn.style = "display: inline-block;float:right";
+              let newdelete = document.createElement("a");
+              newdelete.className = "btn white-text red";
+              newdelete.style = "display: inline-block;float:right";
               newplot.innerText = "Plot";
-              newlearn.innerText="Learn";
-              newdelete.innerText = "Delete";newdelete.id = data[key]['filename'];newdelete.onclick=delete_file;
+              newlearn.innerText = "Learn";
+              newdelete.innerText = "Delete";
+              newdelete.id = data[key]["filename"];
+              newdelete.onclick = delete_file;
               newdiv.appendChild(newp);
               newdiv.appendChild(newdelete);
               newdiv.appendChild(newlearn);
               newdiv.appendChild(newplot);
-              div.appendChild(newdiv);
+              div.insertBefore(newdiv, document.getElementById("unf"));
             }
           }
         }
       }
     }
-    function check_login_state(){
+    function check_login_state() {
       var sender_object = {
         xhr: new XMLHttpRequest(),
-        send:function(){
-          this.xhr.open("POST",url+"/api/uac/v1/check",true);
+        send: function() {
+          this.xhr.open("POST", url + "/api/uac/v1/check", true);
           this.xhr.onreadystatechange = this.callback;
           this.xhr.withCredentials = true;
           this.xhr.send();
         },
-        callback:function(){
-          if (this.readyState == 4){
-            if (this.status == 202){
+        callback: function() {
+          if (this.readyState == 4) {
+            if (this.status == 202) {
               //no problems
-              document.getElementById("main").style={};
+              document.getElementById("main").style = {};
               load_table();
             }
-            if (this.status == 403){
+            if (this.status == 403) {
               alert("Error");
-              document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-              document.cookie = "loginstring=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-              window.location.href="/login";
+              document.cookie =
+                "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              document.cookie =
+                "loginstring=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              window.location.href = "/login";
             }
-            if(this.status == 401){
+            if (this.status == 401) {
               alert("Error");
-              document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-              document.cookie = "loginstring=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-              window.location.href="/login";
+              document.cookie =
+                "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              document.cookie =
+                "loginstring=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              window.location.href = "/login";
             }
-            if (this.status == 204){
+            if (this.status == 204) {
               //not yet logged in. no problem.
               alert("You haven't logged in yet");
-              document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-              document.cookie = "loginstring=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              document.cookie =
+                "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              document.cookie =
+                "loginstring=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
               console.log("Please login");
               window.location.href = "/login";
             }
           }
         }
-      }
+      };
       sender_object.send();
     }
     check_login_state();
   }
   render() {
-    
     return (
       <>
-        <main id="main" style={{display:"none"}}>
+        <main id="main" style={{ display: "none" }}>
           <h3 className="center">Dashboard</h3>
-          <a className="btn green">
-            <NavLink to="/upload" className="white-text">
-              Upload New File
-            </NavLink>
-          </a>
-          <div className="container" id="file_list">
-
+          <div className="container">
+            <div className="row" id="file_list">
+              <div id="unf" className="col s12 center">
+                <a className="btn green">
+                  <NavLink to="/upload" className="white-text">
+                    Upload New File
+                  </NavLink>
+                </a>
+              </div>
+            </div>
           </div>
         </main>
       </>
