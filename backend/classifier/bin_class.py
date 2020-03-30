1a2a9af273177bef46ba0476e9186920bc80aa96
Sarang Ravindra <sarang7822ravindra@gmail.com>
2020-03-30 19:46:47 +0530

    Training up and running with small bug

--- backend/classifier/bin_class.py
+++ backend/classifier/bin_class.py
@@ -6,6 +6,15 @@ import pymongo
 from datetime import date
 import json
 import hashlib
+from io import StringIO
+import pandas as pd
+import numpy as np
+from keras import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+
+model = Sequential()
+# First Hidden Layer
 
 app = flask.Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -38,6 +47,52 @@ def get_features(filename):
         cols = data.split('\n')[0].split(',')
         return json.dumps({'features': cols})
 
+
+@app.route("/api/bin_class/v1/train/<filename>", methods=["PUT"])
+def train(filename):
+    data = get_file_data(flask.request.cookies['username'], filename)
+    df = pd.read_csv(StringIO(data))
+    features = flask.request.form['features'].split(',')
+    target = flask.request.form['target']
+    print(len(features))
+    Y = np.array(df[target])
+    t_data = np.array(df[features])
+    print(t_data)
+    sc = StandardScaler()
+    X = sc.fit_transform(t_data)
+
+    global model
+    classifier = Sequential()
+    classifier.add(Dense(4, activation='relu', input_dim=len(features)))
+    classifier.add(Dense(4, activation='relu'))
+    classifier.add(Dense(1, activation='sigmoid'))
+    classifier.compile(
+        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    classifier.fit(X, Y, batch_size=2, epochs=10)
+    model = classifier
+    print(model.predict(np.array([[6, 100, 0, 33.6, 0.627]])))
+    return flask.Response(status=200)
+
+
+'''@app.route("/api/bin_class/v1/query/<filename>", methods=["POST"])
+def train(filename):
+    features = flask.request.form['features'].split(',')
+    target = flask.request.form['target']
+    print(len(features))
+    Y = np.array(df[target])
+    t_data = np.array(df[features])
+    sc = StandardScaler()
+    X = sc.fit_transform(t_data)
+    global classifier
+    classifier = Sequential()
+    classifier.add(Dense(4, activation='relu', input_dim=len(features)))
+    classifier.add(Dense(4, activation='relu'))
+    classifier.add(Dense(1, activation='sigmoid'))
+    classifier.compile(
+        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    classifier.fit(X, Y, batch_size=2, epochs=100)
+    return flask.Response(status=200)
+'''
 
 if __name__ == '__main__':
     app.run(port=7001)
--- frontend/src/components/forms/TrainForm.js
+++ frontend/src/components/forms/TrainForm.js
@@ -5,6 +5,12 @@ import { NavLink } from "react-router-dom";
 import gen_request from "../../libfx/gen_request";
 
 class TrainForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: this.props.file
+    };
+  }
   componentDidMount() {
     var url = "http://localhost:4000";
     let file = this.props.file;
@@ -28,12 +34,39 @@ class TrainForm extends React.Component {
     }
     function callback() {
       if (this.readyState == 4) {
-        console.log("hi");
         if (this.status == 200) {
           let data = JSON.parse(this.response);
-          console.log(data);
-
-          document.getElementById("unf").innerHTML = String(data["features"]);
+          for (let i = 0; i < data["features"].length; i++) {
+            var p = document.createElement("p");
+            var label = document.createElement("label");
+            var span = document.createElement("span");
+            span.innerHTML = data["features"][i];
+            var radio = document.createElement("input");
+            radio.setAttribute("type", "radio");
+            radio.setAttribute("class", "target");
+            radio.setAttribute("name", "target");
+            radio.setAttribute("value", data["features"][i]);
+            label.appendChild(radio);
+            label.appendChild(span);
+            p.appendChild(label);
+            document.getElementById("target").appendChild(p);
+          }
+          for (let i = 0; i < data["features"].length; i++) {
+            var p = document.createElement("p");
+            var label = document.createElement("label");
+            var span = document.createElement("span");
+            span.innerHTML = data["features"][i];
+            var check = document.createElement("input");
+            check.setAttribute("type", "checkbox");
+            check.setAttribute("class", "feat");
+            check.setAttribute("name", data["features"][i]);
+            check.setAttribute("value", data["features"][i]);
+            label.appendChild(check);
+            label.appendChild(span);
+            p.appendChild(label);
+            document.getElementById("feature").appendChild(p);
+          }
+          //document.getElementById("unf").innerHTML = String(data["features"]);
         }
       }
     }
@@ -62,13 +95,68 @@ class TrainForm extends React.Component {
     check_login_state();
   }
   render() {
+    var url =
+      "http://localhost:7001/api/bin_class/v1/train/" + String(this.state.file);
+    function train() {
+      let feat = document.getElementsByClassName("feat");
+      var features = [];
+      for (let i = 0; i < feat.length; i++) {
+        if (feat[i].checked) {
+          features.push(feat[i].value);
+        }
+      }
+      if (features.length == 0) {
+        alert("Please select atleast one feature. Don't be dumb");
+        return;
+      }
+      var target = document.querySelector('input[name="target"]:checked');
+      if (target == null) {
+        alert("The fuck do you want me to learn?");
+        return;
+      }
+      var target = document.querySelector('input[name="target"]:checked').value;
+      var sender_object = {
+        xhr: new XMLHttpRequest(),
+        send: function() {
+          this.xhr.open("PUT", url, true);
+          this.xhr.onreadystatechange = this.callback;
+          this.xhr.withCredentials = true;
+          let data = new FormData();
+          console.log(features);
+          data.append("features", features);
+          data.append("target", target);
+          this.xhr.send(data);
+        },
+        callback: function() {
+          if (this.readyState == 4) {
+            if (this.status == 202) {
+              alert("Training Complete");
+            }
+          }
+        }
+      };
+      sender_object.send();
+    }
+
     return (
       <>
         <main id="main" style={{ display: "none" }}>
-          <h3 className="center">Train</h3>
+          <h3 className="center">Learn</h3>
           <div className="container">
             <div className="row">
-              <div id="unf" className="col s12 center"></div>
+              <div id="unf" className="col s12 center">
+                <form action="#">
+                  <div id="feature" className="col s6">
+                    <h5 className="center">Select Feature(s)</h5>
+                  </div>
+                  <div id="target" className="col s6">
+                    <h5 className="center">Select Target</h5>
+                  </div>
+                  <a className="btn waves-effect green" onClick={train}>
+                    Learn
+                  </a>
+                </form>
+              </div>
             </div>
           </div>
         </main>
